8f05758923efa9e2a12a61ca9fd1c413d06dfa33
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-07-08 22:53:02 -0300

    Included uuid field and migrated customUser model

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django_cpf_cnpj.fields import CPFField
+import uuid
+from apps.core.models import Auditoria
 
 
 # Atributos customizados devem ser adicionados aqui
-class CustomUser(AbstractUser):
+class CustomUser(AbstractUser, Auditoria):
+    Id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     cpf = CPFField(masked=True, null=True, blank=True, verbose_name="CPF")
-    matricula = models.PositiveBigIntegerField(max_length=6, null=True, blank=True, verbose_name="Matrícula")
+    matricula = models.PositiveBigIntegerField(null=True, blank=True, verbose_name="Matrícula")
 
