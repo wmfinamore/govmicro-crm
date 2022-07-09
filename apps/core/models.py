c7b34fc5cdb8e3878d9952431326b6cdef9fbaa1
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-07-08 22:23:13 -0300

    Implemented Auditoria class model

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from simple_history.models import HistoricalRecords
 
-# Create your models here.
+
+class Auditoria(models.Model):
+    data_inclusao = models.DateField(auto_now_add=True, editable=False, verbose_name="Data de Inclusão")
+    hora_inclusao = models.TimeField(auto_now_add=True, editable=False, verbose_name="Hora de Inclusão")
+    data_alteracao = models.DateTimeField(auto_now=True, editable=False, verbose_name="Data de Alteração")
+    hora_alteracao = models.TimeField(auto_now=True, editable=False, verbose_name="Hora de Alteração")
+    history = HistoricalRecords(inherit=True)
+
+    class Meta:
+        abstract = True
