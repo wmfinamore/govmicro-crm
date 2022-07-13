dc92db6b81ce13e880c9bd164f8cc85b6acea272
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-07-13 19:57:24 -0300

    Implemented tests for CustomUser model and Signup view

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,49 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+    """
+    Teste de criação de usuário básico e superadmin
+    """
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='will',
+            email='will@mail.com',
+            password='testpass123'
+        )
+        self.assertEqual(user.username, 'will')
+        self.assertEqual(user.email, 'will@mail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superadmin',
+            email='superadmin@mail.com',
+            password='testpass123'
+        )
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@mail.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+
+
+class SignupTests(TestCase):
+    """
+    Teste de rotas e templates para a página de criação de novos usuários
+    """
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'registration/signup.html')
+        self.assertContains(self.response, 'Crie sua nova conta!')
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
