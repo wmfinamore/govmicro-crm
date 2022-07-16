5ab4ce10da49a6e8832c3ebed1cea3d0b22aed14
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-07-15 23:10:55 -0300

    removed unused code from views and urls in accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,1 @@
-from django.urls import path
-from .views import SignupView
 
-
-urlpatterns = [
-    path('signup', SignupView.as_view(), name='signup'),
-]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,2 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from .forms import CustomUserCreationForm
 
 
-class SignupView(CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
