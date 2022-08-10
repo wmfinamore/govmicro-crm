f3ebb2dfe34d04e6e6d6214dac5dbee5393074e1
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-08-09 23:28:34 -0300

    Implemented Assunto model with mptt package

--- apps/assuntos/models.py
+++ apps/assuntos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+from apps.core.models import Auditoria
 
-# Create your models here.
+
+class Assunto(MPTTModel, Auditoria):
+    nome = models.CharField(max_length=100, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def __str__(self):
+        return self.nome
