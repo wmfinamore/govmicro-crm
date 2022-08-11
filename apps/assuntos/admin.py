096dc8eb3068709b63d1d5839031a087303658cb
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-08-10 23:26:06 -0300

    Change class AssuntoAdmin to use Draggable mptt

--- apps/assuntos/admin.py
+++ apps/assuntos/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import Assunto
-from mptt.admin import MPTTModelAdmin
+from mptt.admin import DraggableMPTTAdmin
 
 
-class AssuntoAdmin(MPTTModelAdmin):
-    list_display = ['id', 'nome', 'level']
+class AssuntoAdmin(DraggableMPTTAdmin):
+    list_display = ['tree_actions', 'indented_title', 'level']
+    list_per_page = 100
+    expand_tree_by_default = True
 
 
 admin.site.register(Assunto, AssuntoAdmin)
