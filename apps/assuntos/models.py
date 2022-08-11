6f8557a71917a8d7adb092a2f8a63fa9e002d873
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-08-10 23:06:51 -0300

    Changed model Assunto

--- apps/assuntos/models.py
+++ apps/assuntos/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
-from apps.core.models import Auditoria
 
 
-class Assunto(MPTTModel, Auditoria):
+class Assunto(MPTTModel):
     nome = models.CharField(max_length=100, unique=True)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
 
