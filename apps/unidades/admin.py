494659302060f272c76380aab06f7315ed598085
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-08-11 22:05:55 -0300

    Included model Unidade in admin module as Draggable Mptt

--- apps/unidades/admin.py
+++ apps/unidades/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Unidade
+from mptt.admin import DraggableMPTTAdmin
 
-# Register your models here.
+
+class UnidadeAdmin(DraggableMPTTAdmin):
+    list_display = ['tree_actions', 'indented_title', 'level']
+
+
+admin.site.register(Unidade, UnidadeAdmin)
