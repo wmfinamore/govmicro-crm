d0a76824fbcd90d7d0eda368af225e40d435de28
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-08-11 22:05:07 -0300

    Implemented and migrated model Unidade

--- apps/unidades/models.py
+++ apps/unidades/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+from simple_history.models import HistoricalRecords
 
-# Create your models here.
+
+class Unidade(MPTTModel):
+    nome = models.CharField(max_length=100, verbose_name='Nome da Unidade')
+    sigla = models.CharField(max_length=10, null=True, blank=True, verbose_name='Sigla')
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    ativo = models.BooleanField(default=True)
+    history = HistoricalRecords(excluded_fields=['lft', 'rght', 'tree_id', 'level'])
+
+    def __str__(self):
+        return str(self.sigla) + ' - ' + self.nome
