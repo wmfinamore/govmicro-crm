08b70dce2ca5b4041a9ae42698d0a1b1a5461d81
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-08-14 19:06:27 -0300

    Implemented test to create and retrieve instance of assunto model

--- apps/assuntos/tests.py
+++ apps/assuntos/tests.py
@@ -1,3 +1,21 @@
+from .models import Assunto
 from django.test import TestCase
 
-# Create your tests here.
+
+class AssuntoTests(TestCase):
+    """
+    Teste de criação de assunto
+    """
+    def test_create_assunto(self):
+        assunto = Assunto.objects.create(
+            nome='Assunto Primário',
+        )
+        self.assertEqual(assunto.nome, 'Assunto Primário')
+        self.assertEqual(assunto.level, 0)
+
+        assunto_secundario = Assunto.objects.create(
+            nome='Assunto Secundário',
+            parent=assunto
+        )
+        self.assertEqual(assunto_secundario.nome, 'Assunto Secundário')
+        self.assertEqual(assunto_secundario.level, 1)
