9b37a7dbb58f8c19dc54dd6b03df9b546a68ca38
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-08-14 19:14:21 -0300

    Implemented test to create and retrieve instance of unidade class

--- apps/unidades/tests.py
+++ apps/unidades/tests.py
@@ -1,3 +1,29 @@
+from .models import Unidade
 from django.test import TestCase
 
-# Create your tests here.
+
+class UnidadeTests(TestCase):
+    """
+    Teste de criação de unidade
+    """
+    def test_create_unidade(self):
+        unidade = Unidade.objects.create(
+            nome='Secretaria',
+            sigla='SE',
+            ativo=True,
+        )
+        self.assertEqual(unidade.nome, 'Secretaria')
+        self.assertEqual(unidade.sigla, 'SE')
+        self.assertTrue(unidade.ativo)
+        self.assertEqual(unidade.level, 0)
+
+        unidade_secundaria = Unidade.objects.create(
+            nome='Divisão',
+            sigla='DI',
+            ativo=True,
+            parent=unidade
+        )
+        self.assertEqual(unidade_secundaria.nome, 'Divisão')
+        self.assertEqual(unidade_secundaria.sigla, 'DI')
+        self.assertTrue(unidade_secundaria.ativo)
+        self.assertEqual(unidade_secundaria.level, 1)
