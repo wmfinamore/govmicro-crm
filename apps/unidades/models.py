b93a12a3b3ed2ae7f21972598a6b94ec60d4a487
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-08-14 23:06:01 -0300

    Added field funcionarios in Unidade model class

--- apps/unidades/models.py
+++ apps/unidades/models.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 from simple_history.models import HistoricalRecords
+
+User = get_user_model()
 
 
 class Unidade(MPTTModel):
@@ -8,7 +11,11 @@ class Unidade(MPTTModel):
     sigla = models.CharField(max_length=10, null=True, blank=True, verbose_name='Sigla')
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
     ativo = models.BooleanField(default=True)
+    funcionarios = models.ManyToManyField(User, blank=True, limit_choices_to={'is_staff': True}
+                                          , related_name='unidades'
+                                         )
     history = HistoricalRecords(excluded_fields=['lft', 'rght', 'tree_id', 'level'])
 
     def __str__(self):
         return str(self.sigla) + ' - ' + self.nome
+
