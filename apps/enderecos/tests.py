38b0c18f4dd9af82f9b193d49a565beaef5910c9
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-08-18 23:26:10 -0300

    Implemented unit test for Endereco model

--- apps/enderecos/tests.py
+++ apps/enderecos/tests.py
@@ -1,3 +1,19 @@
+from .models import Endereco
 from django.test import TestCase
 
-# Create your tests here.
+
+class EnderecoTests(TestCase):
+    """
+    Teste de criação de Endereco
+    """
+    def test_create_endereco(self):
+        endereco = Endereco.objects.create(
+            endereco='Avenida Engenheiro Carlos Reinaldo Mendes',
+            bairro='Alto da Boa Vista',
+            cep='18013280',
+        )
+        self.assertEqual(endereco.endereco, 'Avenida Engenheiro Carlos Reinaldo Mendes')
+        self.assertEqual(endereco.bairro, 'Alto da Boa Vista')
+        self.assertEqual(endereco.cep, '18013280')
+        self.assertEqual(endereco.cidade, 'Sorocaba')  # test default value
+        self.assertEqual(endereco.estado, 'SP')  # test default value
