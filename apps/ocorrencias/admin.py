57c44345909a8ed15356749c83f916f499b2e3ea
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-08-21 18:02:33 -0300

    Implemented and registered OcorrenciaAdmin

--- apps/ocorrencias/admin.py
+++ apps/ocorrencias/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
+from .models import Ocorrencia
 
-# Register your models here.
+
+class OcorrenciaAdmin(SimpleHistoryAdmin):
+    autocomplete_fields = ['solicitante', 'assunto', 'endereco']
+
+
+admin.site.register(Ocorrencia, OcorrenciaAdmin)
