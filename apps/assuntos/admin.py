76dcf2ea33ed8bfebd1dbfb5b5b8a8546371a6e2
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-08-22 21:34:26 -0300

    Included folha field in list fields for AssuntoAdmin

--- apps/assuntos/admin.py
+++ apps/assuntos/admin.py
@@ -5,7 +5,7 @@ from mptt.admin import DraggableMPTTAdmin
 
 class AssuntoAdmin(DraggableMPTTAdmin):
     search_fields = ['nome']
-    list_display = ['tree_actions', 'indented_title', 'level']
+    list_display = ['tree_actions', 'indented_title', 'level', 'folha']
     list_per_page = 100
     expand_tree_by_default = True
 
