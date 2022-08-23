009e2d2cafc5aa145c344ab59989628a529a103c
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-08-22 21:33:49 -0300

    Included folha field and migrated changes.Implemented signal to update folha field

--- apps/assuntos/models.py
+++ apps/assuntos/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Assunto(MPTTModel):
     nome = models.CharField(max_length=100, unique=True)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    folha = models.BooleanField(default=True, editable=False)
 
     def __str__(self):
         return self.nome
+
+
+@receiver(post_save, sender=Assunto)
+def update_folha(sender, instance, **kwargs):
+    Assunto.objects.filter(id=instance.id). \
+        update(folha=instance.is_leaf_node())
+    # ancestors = instance.get_ancestors()
+    # for ancestor in ancestors:
+    #     ancestor.objects.update(folha=ancestor.is_leaf_node())
