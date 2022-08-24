aacbe2de8e198b6e56355bf61478cf44045f681d
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-08-23 21:15:22 -0300

    Included field ano. Override save method to set numero

--- apps/ocorrencias/models.py
+++ apps/ocorrencias/models.py
@@ -5,6 +5,7 @@ from apps.assuntos.models import Assunto
 from apps.enderecos.models import Endereco
 from apps.unidades.models import Unidade
 import uuid
+from datetime import date
 
 
 User = get_user_model()
@@ -14,11 +15,23 @@ class Ocorrencia(Auditoria):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     numero = models.PositiveBigIntegerField(null=True, blank=True)
     solicitante = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, limit_choices_to={'is_staff': False}, )
+    ano = models.PositiveIntegerField(default=date.today().strftime("%Y"), editable=False)
     assunto = models.ForeignKey(Assunto, on_delete=models.PROTECT, limit_choices_to={'folha': True}, )
     endereco = models.ForeignKey(Endereco, on_delete=models.PROTECT, verbose_name='Endereço')
     anonimo = models.BooleanField(default=False, verbose_name='Anônimo?')
     descricao_ocorrencia = models.TextField(null=True, blank=True, verbose_name='Descrição da Ocorrência')
-    unidade_atual = models.ForeignKey(Unidade, blank=True, on_delete=models.PROTECT, editable=False)
+    unidade_atual = models.ForeignKey(Unidade, blank=True, null=True, on_delete=models.PROTECT, editable=False)
+
+    def save(self, *args, **kwargs):
+        if self.numero is not None:
+            pass
+        else:
+            numero = Ocorrencia.objects.filter(ano=date.today().strftime("%Y")).order_by('-numero').first()
+            if numero:
+                self.numero = int(numero.numero) + 1
+            else:
+                self.numero = 1
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return str(self.solicitante) + ' - ' + str(self.assunto) + ' - ' - str(self.endereco)
+        return str(self.solicitante) + ' - ' + str(self.assunto) + ' - ' + str(self.endereco)
