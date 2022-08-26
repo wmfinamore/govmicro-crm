519b3e8cbd815a199915923b8262cec204511996
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-08-25 22:02:36 -0300

    Included unidade as autocomplete field in assuntoAdmin class

--- apps/assuntos/admin.py
+++ apps/assuntos/admin.py
@@ -8,6 +8,7 @@ class AssuntoAdmin(DraggableMPTTAdmin):
     list_display = ['tree_actions', 'indented_title', 'level', 'folha']
     list_per_page = 100
     expand_tree_by_default = True
+    autocomplete_fields = ['unidade']
 
 
 admin.site.register(Assunto, AssuntoAdmin)
