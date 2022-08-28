3e1498d7c7b39d9e5420e2c47ef53f07ac55e568
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-08-28 15:27:41 -0300

    Implemented bases for OcorrenciaTest class

--- apps/ocorrencias/tests.py
+++ apps/ocorrencias/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Ocorrencia
+from apps.enderecos.models import Endereco
+from apps.assuntos.models import Assunto
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class OcorrenciaTests(TestCase):
+    def setUp(self):
+        pass
+
+    def test_ocorrencia_listing(self):
+        pass
