c83e16bedc0d50e784f30c4e5fc5ea02d07af301
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-09-18 19:49:20 -0300

    Implemented filter for unidade where usuario is related

--- apps/ocorrencias/admin.py
+++ apps/ocorrencias/admin.py
@@ -6,5 +6,16 @@ from .models import Ocorrencia
 class OcorrenciaAdmin(SimpleHistoryAdmin):
     autocomplete_fields = ['solicitante', 'assunto', 'endereco']
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        data = []
+        unidades = request.user.unidades.all()
+        for unidade in unidades:
+            data.append(unidade.id)
+        if request.user.is_superuser:
+            return qs
+        else:
+            return qs.filter(unidade_atual__in=data)
+
 
 admin.site.register(Ocorrencia, OcorrenciaAdmin)
